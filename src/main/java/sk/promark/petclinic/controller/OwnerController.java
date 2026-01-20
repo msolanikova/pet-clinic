0f5616f0cd9a22ac0d56265caf0130784fa12ada
Marketa Solanikova <solanikova@promark.sk>
2026-01-20 14:33:33 +0100

    add param validation to /owners; use default values to improve null handling

--- src/main/java/sk/promark/petclinic/controller/OwnerController.java
+++ src/main/java/sk/promark/petclinic/controller/OwnerController.java
@@ -1,11 +1,14 @@
 package sk.promark.petclinic.controller;
 
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sk.promark.petclinic.model.OwnersResponse;
 import sk.promark.petclinic.service.OwnerService;
 
+@RequestMapping("/owners")
 @RestController
 public class OwnerController {
 
@@ -15,10 +18,9 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping("/owners")
-    public OwnersResponse getOwners(@RequestParam(name = "page", required = false) Integer page, @RequestParam(name = "size", required = false) Integer size) {
-        page = page == null ? 0 : page;
-        size = size == null ? 2 : size;
+    @GetMapping
+    public OwnersResponse getOwners(@RequestParam(name = "page", defaultValue = "0") @Min(0) int page,
+                                    @RequestParam(name = "size", defaultValue = "2") @Min(1) int size) {
 
         return ownerService.getOwners(page, size);
     }
