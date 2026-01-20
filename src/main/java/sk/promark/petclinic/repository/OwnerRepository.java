747a0a7202c2ad4f249f206d75fb3f8f32ad5c55
Marketa Solanikova <solanikova@promark.sk>
2026-01-20 14:32:42 +0100

    fix id type in owner repository and extend jpa repository that supports pagination and sorting by default

--- src/main/java/sk/promark/petclinic/repository/OwnerRepository.java
+++ src/main/java/sk/promark/petclinic/repository/OwnerRepository.java
@@ -1,10 +1,7 @@
 package sk.promark.petclinic.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sk.promark.petclinic.entity.Owner;
 
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
-    Page<Owner> findAll(Pageable pageable);
+public interface OwnerRepository extends JpaRepository<Owner, String> {
 }
